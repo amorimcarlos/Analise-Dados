5b2a570cb4a27d33772baf65b44a65d30155250e
Carlos Vinícius Rodrigues Amorim <carlosrodriguesreal@gmail.com>
2025-10-25 12:47:05 -0300

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,10 @@
 import streamlit as st
 from análise import *
 
-st.title 
+st.title("Como o consumo de álcool pode afetar diretamente o desempenho escolar de adolescentes")
+
+descricao = st.container(border= True)
+descricao.text("Esta aplicação foi desenvolvida para que pudesse facilitar a visualização dos dados implementados")
+
+st.subheader("Visualização das médias das notas pela idade.")
+grafico1 = media_notas_idade()
